221dc08ab7e35096b556abd96865ea5d7efb21fd
Olga Filipova <chudaol@gmail.com>
2022-03-31 01:44:10 +0200

    added button to help ukraine

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
 <template>
-  <v-row justify="center" align="center">
+  <v-row justify="center" align="center" class="mt-12 pt-12">
     <v-col cols="12" sm="8" md="6">
       <v-card
         style="background: linear-gradient(blue, yellow)"
       >
         <v-card-title class="headline text-center" style="word-break: break-word; font-size: 10em;">
-          It has been {{ daysFromWar }} days since the full-scale russian war in Ukraine started.
+          <h2>
+            It has been <span class="font-weight-bold">{{ daysFromWar }}</span> days since the full-scale russian war in Ukraine started.
+          </h2>
         </v-card-title>
+        <v-card-text class="text-center">
+          The war started on <strong>24.02.2022</strong>
+        </v-card-text>
+        <v-card-actions class="justify-center">
+          <v-btn
+            href="https://bit.ly/help-ua-now"
+          >
+            Help Ukraine
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
